729bb756594d362ad4bf17a998ae727d8c0334f5
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-08-19 00:51:50 -0300

    Main view controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,7 @@ module com.workshop {
     requires javafx.fxml;
 
     opens com.workshop to javafx.fxml;
+    opens com.workshop.gui.controller to javafx.fxml;
     exports com.workshop;
+    exports com.workshop.gui.controller to javafx.fxml;
 }
